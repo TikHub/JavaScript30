5aebd66622e8172b78b7b995bce88113b74353c1
Tigran <soghomonyantigran@gmail.com>
2019-07-07 01:02:03 +0400

    24: complete

---
+++ solutions/24 - Sticky Nav/app.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector("#main");
+const topOfNav = nav.offsetTop;
+
+window.addEventListener("scroll", fixNav);
+
+function fixNav() {
+  console.log(topOfNav, window.scrollY);
+  if (window.scrollY >= topOfNav) {
+    document.body.classList.add("fixed-nav");
+    document.body.style.paddingTop = nav.offsetHeight + "px";
+  } else {
+    document.body.classList.remove("fixed-nav");
+    document.body.style.paddingTop = 0;
+  }
+}
